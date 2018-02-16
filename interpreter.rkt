70a31cdfb287aaae946039ad58d848fb1ce4ce7f
Jonathan Duffy <jcduffy5@gmail.com>
2018-02-16 12:19:35 -0500

    Added Function arith-eval
    Function that takes a simple or compound arithmetic expression (* + - /)
    and returns the proper return value.

--- interpreter.rkt
+++ interpreter.rkt
@@ -5,4 +5,25 @@
 ;call (interpret "TestCode.txt")
 (define interpret
   (lambda (filename)
-    (parser filename)))
+    (arith_eval (parser filename))))
+
+;arith_eval - Function that takes a simple or compound arithmetic expression (* + - /) and returns the proper return value.
+(define arith_eval
+  (lambda (expr)
+    (cond ((number? expr) expr);if single number, return it.
+    (else (let ((op (car expr))
+                (arg1 (arith_eval (cadr expr)))
+                (arg2 (arith_eval (caddr expr))))
+            (cond ((eq? op '*)
+                   (* arg1 arg2))
+                  ((eq? op '+)
+                   (+ arg1 arg2))
+                  ((eq? op '-)
+                   (- arg1 arg2))
+                  ((eq? op '/)
+                   (/ arg1 arg2))
+                  (else
+                   (error "Invalid operation in: " expr))))))))
+                  
+          
+           
