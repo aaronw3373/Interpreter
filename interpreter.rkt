4246d31bb0a952bf03de8bfa22e964b4345fbeeb
Aaron Weinberg <aaron.a.weinberg@case.edu>
2018-02-16 14:27:48 -0500

    Added a function to take each operation from the parser and forward it to a function to handle it

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,13 +1,55 @@
 #lang racket
 (require "simpleParser.scm")
 
+;atom? function to tell if something is an atom
+(define (atom? x) (not (or (pair? x) (null? x) (vector? x))))
+
 ;Main Function that takes a filename calles parser on it, evaluates the parse tree and returns the proper value (or error if a variable is used before declared).
 ;call (interpret "TestCode.txt")
 (define interpret
   (lambda (filename)
-    ((parser filename))))
+    (Forward_OP (car (parser filename)))));Test first expression for now
 
-;arith_eval - Function that takes a simple or compound arithmetic expression (* + - /) and returns the proper return value.
+;Takes a single operation in form of list (operation args1 args2 etc...) ard forwards the list to the correct operation
+(define Forward_OP 
+  (lambda (lis)
+    (cond
+      ((null? lis) '())
+      ((eq? (car lis) list?) (Forward_OP (car lis)))
+      ((atom? (car lis))  (cond
+                             ((eq? (car lis) 'var)      (declaration_OP lis))
+                             ((eq? (car lis) '=)        (assignment_OP lis))
+                             ((eq? (car lis) 'return)   (return_OP lis))
+                             ((eq? (car lis) 'if)       (if_OP lis))
+                             ((eq? (car lis) 'while)    (while_OP lis))))
+      (else (error "Invalid single operation in: " lis)))))
+
+;Declaration (var variable (value optional))
+(define declaration_OP
+  (lambda (lis)
+    (car lis)))
+
+;Assignment (= variable expression)
+(define assignment_OP
+  (lambda (lis)
+    (car lis)))
+
+;Return (return expression)
+(define return_OP
+  (lambda (lis)
+    (arith_eval (cadr lis))))
+
+;if statement (if conditional then-statement optional-else-statement)
+(define if_OP
+  (lambda (lis)
+    (car lis)))
+
+;while statement (while conditional body-statement)
+(define while_OP
+  (lambda (lis)
+    (car lis)))
+
+;arith_eval - Function that takes a simple or compound arithmetic expression (* + - / %) and returns the proper return value. (Doesn't take variables yet)
 (define arith_eval
   (lambda (expr)
     (cond ((number? expr) expr);if single number, return it.
@@ -22,6 +64,8 @@
                    (- arg1 arg2))
                   ((eq? op '/)
                    (/ arg1 arg2))
+                  ((eq? op '%)
+                   (remainder arg1 arg2))                  
                   (else
                    (error "Invalid operation in: " expr)))))))) ;throw error if operator isn't one of those.
                   
