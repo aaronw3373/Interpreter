8f6b341dc207cbf4df82c536a932a12f16e0e309
Aaron Weinberg <aaron.a.weinberg@case.edu>
2018-02-19 17:39:48 -0500

    got rid of let in arith_eval connamacher wouldn't let us do let

--- interpreter.rkt
+++ interpreter.rkt
@@ -33,8 +33,18 @@
 ;else run M_Forward_OP on the next expr in the program
 (define M_Program
   (lambda (program state return_b return_v)
-    (cons (car (M_Forward_OP (car program) state return_b return_v)) (cons (cadr (M_Forward_OP (car program) state return_b return_v)) (cons (caddr (M_Forward_OP (car program) state return_b return_v)) '())))
-    ))
+    (cond
+      ((or (eq? return_b #t) (null? program)) (cons state (cons return_b (cons return_v '())))) ; nothing to evaluate or change so return as is
+      (else  (cons (car (M_Forward_OP (car program) state return_b return_v))
+                   (cons (cadr (M_Forward_OP (car program) state return_b return_v))
+                         (cons (caddr (M_Forward_OP (car program) state return_b return_v)) '()))))
+    )))
+
+
+
+    ;  (else (cons (car (M_program (cdr p) (car (M_state (car p) s r_b r_v)) (cadr (M_state (car p) s r_b r_v)) (caddr (M_state (car p) s r_b r_v))))
+     ;             (cons (cadr (M_program (cdr p) (car (M_state (car p) s r_b r_v)) (cadr (M_state (car p) s r_b r_v)) (caddr (M_state (car p) s r_b r_v))))
+      ;                  (cons (caddr (M_program (cdr p) (car (M_state (car p) s r_b r_v)) (cadr (M_state (car p) s r_b r_v)) (caddr (M_state (car p) s r_b r_v)))) '())))))
 
 
 ;there wasn't a != operator, so now there is.
@@ -84,22 +94,25 @@
 ;arith_eval - Function that takes a simple or compound arithmetic expression (* + - / %) and returns the proper return value and the state (Doesn't take variables yet)
 (define arith_eval
   (lambda (expr state)
-    (cond ((number? expr) expr);if single number, return it.
-    (else (let ((op (car expr));else, take arguments, handle compound expressions if necessary
-                (arg1 (arith_eval (cadr expr) state))
-                (arg2 (arith_eval (caddr expr) state)))
-            (cond ((eq? op '*) ;snarf underlying Scheme operators.
-                   (cons state (cons (* arg1 arg2) '())))
-                  ((eq? op '+)
-                   (cons state (cons (+ arg1 arg2) '())))
-                  ((eq? op '-)
-                   (cons state (cons (- arg1 arg2) '())))
-                  ((eq? op '/)
-                   (cons state (cons (/ arg1 arg2) '())))
-                  ((eq? op '%)
-                   (cons state (cons (remainder arg1 arg2) '())) )                
+    (cond
+      ((number? expr) (cons state (cons expr '())));if single number, return it.
+      (else 
+            (cond ((eq? (op expr) '*) ;snarf underlying Scheme operators.
+                   (cons state (cons (* (cadr(arith_eval (arg1 expr) state)) (cadr(arith_eval (arg2 expr) state))) '())))
+                  ((eq? (op expr) '+)
+                   (cons state (cons (+ (cadr(arith_eval (arg1 expr) state)) (cadr(arith_eval (arg2 expr) state))) '())))
+                  ((eq? (op expr) '-)
+                   (cons state (cons (- (cadr(arith_eval (arg1 expr) state)) (cadr(arith_eval (arg2 expr) state))) '())))
+                  ((eq? (op expr) '/)
+                   (cons state (cons (/ (cadr(arith_eval (arg1 expr) state)) (cadr(arith_eval (arg2 expr) state))) '())))
+                  ((eq? (op expr) '%)
+                   (cons state (cons (remainder (cadr(arith_eval (arg1 expr) state)) (cadr(arith_eval (arg2 expr) state))) '())) )                
                   (else
-                   (error "Invalid operation in: " expr)))))))) ;throw error if operator isn't one of those.
+                   (error "Invalid operation in: " expr))))))) ;throw error if operator isn't one of those.
+
+(define op car)
+(define arg1 cadr)
+(define arg2 caddr)
 
 ;list_index - Takes a list and a symbol, returns index of that symbol
 (define list_index
